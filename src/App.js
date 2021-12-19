4d07fe93c7a5132b8fab73e768668f29608e6a47
Jose Luis <josepitarch98@gmail.com>
2021-12-19 22:51:03 +0100

    [FEAT] Add hokk useFormik, components of Formik and hook useField

--- src/App.js
+++ src/App.js
@@ -1,38 +1,57 @@
-import {useFormik } from 'formik'
+import './App.css'
+import { Formik, Field, ErrorMessage, Form } from 'formik'
+import TextInput from './components/TextInput'
+import Checkbox from './components/Checkbox'
+
+const validate = (values) => {
+  const errors = {}
+
+  if(!values.name) {
+    errors.name = 'Requerido'
+  } else if(values.name.length < 5) {
+    errors.name = 'El nombre es demasiado corto'
+  }
+
+  if(!values.lastname) {
+    errors.lastname = 'Requerido'
+  } else if(values.lastname.length < 5) {
+    errors.lastname = 'El apellido es demasiado corto'
+  }
+
+  if(!values.email) {
+    errors.email = 'Requerido'
+  } else if(values.email.length < 5) {
+    errors.email = 'El email es demasiado corto'
+  }
+
+  return errors
+}
+
 function App() {
-  const formik = useFormik({
-    initialValues: {
-      name: '',
-      lastname: '',
-      email: ''
-    },
-    onSubmit: values => console.log(values)
-  })
   return (
-    <form onSubmit={ formik.handleSubmit }>
-      <label>Nombre</label>
-      <input
-        name='name'
-        type='text'
-        onChange={ formik.handleChange }
-        value={ formik.values.name }
-      />
-      <label>Apellido</label>
-      <input
-        name='lastname'
-        type='text'
-        onChange={ formik.handleChange }
-        value={ formik.values.lastname }
-      />
-      <label>Email</label>
-      <input
-        name='email'
-        type='email'
-        onChange={ formik.handleChange }
-        value={ formik.values.email }
-      />
-      <button type='submit'>Enviar</button>
-    </form>
+    <Formik
+      initialValues={{ name: '', lastname: '', email: ''}}
+      validate={ validate }
+      onSubmit={ values => console.log(values) }
+    >
+      <Form>
+        <TextInput name='name' label='Nombre'/>
+        <label>Nombre</label>
+        <Field name='name' type='text'/>
+        <ErrorMessage name='name'/>
+        <label>Apellido</label>
+        <Field name='lastname' type='text'/>
+        <ErrorMessage name='lastname'/>
+        <label>Email</label>
+        <Field name='email' type='email'/>
+        <ErrorMessage name='email'/>
+        <Checkbox name='accept'>
+          Aceptar términos y condiciones
+        </Checkbox>
+
+        <button type='submit'>Enviar</button>
+      </Form>
+    </Formik>
   );
 }
 
---
+++ src/components/TextInput.js
@@ -0,0 +1,17 @@
+import { useField } from 'formik'
+
+const TextInput = ({ label, ...props}) => {
+    const [field, meta] = useField(props)
+
+    console.log({ field, meta})
+
+    return (
+        <div>
+            <label className=''>{ label }</label>
+            <input { ...field }/>
+            { meta.touched && meta.error ? <div>{ meta.error }</div> : null }
+        </div>
+    )
+}
+
+export default TextInput
